465c1ed4e2fb80fc4318e5f1ebfeb40049c2b015
Akexei <cresniov@gmail.com>
2020-10-22 08:07:06 +0300

    homework 1

---
+++ task 1.py
@@ -0,0 +1,8 @@
+xxx=int(input('Please enter the number: '))
+c = xxx+1
+if c < 3:
+    print ("the number is less than 3")
+elif c == 3:
+    print("the number is 3")
+else:
+    print("the number is greater than 3")
